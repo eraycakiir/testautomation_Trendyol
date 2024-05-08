69d67687e16211d061e5ec9f0687e852875d9ff8
Eray Çakır <eraycakir35@gmail.com>
2024-05-08 23:00:35 +0300

    Unnecessary files deleted

--- src/test/java/pages/mainPage.java
+++ src/test/java/pages/mainPage.java
@@ -14,8 +14,7 @@ public class mainPage {
     }
     @AndroidFindBy(xpath = "(//android.widget.ImageView[@resource-id=\"trendyol.com:id/navigation_bar_item_icon_view\"])[5]\n")
     public WebElement myAccountBox;
-    @AndroidFindBy(xpath = "//android.widget.ImageView[@resource-id=\"trendyol.com:id/buttonDismiss\"]")
-    public WebElement dismissButton;
+
     @AndroidFindBy(xpath = "//android.widget.ImageView[@resource-id=\"trendyol.com:id/imageViewTooltipClose\"]\n")
     public WebElement   toolTipCloseButton;
     @AndroidFindBy(xpath = "//android.widget.EditText[@content-desc=\"Ara\"]\n")
